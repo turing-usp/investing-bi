a8634cc2debbfb39393a45503106ece519314b3c
lucas-leme <lucaslssantos99@gmail.com>
2021-04-01 20:52:27 -0300

    Refact: unify get_asset_data_frames

--- portfolio_prices.py
+++ portfolio_prices.py
@@ -5,50 +5,20 @@ import numpy as np
 
 today = datetime.today().strftime("%d/%m/%Y")
 
-def get_stock_data_frames(stocks, country, start_date, end_date):
+def get_assets_data_frames(assets, asset_function, country, start_date, end_date):
     
     data_frames = []
     
-    for stock in stocks:
+    for asset in assets:
         
-        data_frame = inv.get_stock_historical_data(stock=stock,
-                                                   country=country,
-                                                   from_date=start_date,
-                                                   to_date=end_date)
-        
-        data_frames.append(data_frame)
-    
-    return data_frames
-
-def get_funds_data_frames(funds, country, start_date, end_date):
-    
-    data_frames = []
-    
-    for fund in funds:
-        
-        data_frame = inv.get_fund_historical_data(fund=fund,
-                                                  country=country,
-                                                  from_date=start_date,
-                                                  to_date=end_date)
+        data_frame = asset_function(asset,
+                                    country=country,
+                                    from_date=start_date,
+                                    to_date=end_date)
         
         data_frames.append(data_frame)
     
-    return data_frames
-
-def get_etfs_data_frames(etfs, country, start_date, end_date):
-    
-    data_frames = []
-    
-    for etf in etfs:
-        
-        data_frame = inv.get_etf_historical_data(etf=etf,
-                                                 country=country,
-                                                 from_date=start_date,
-                                                 to_date=end_date)
-        
-        data_frames.append(data_frame)
-    
-    return data_frames
+    return data_frames 
 
 def build_multi_index_tuples(header, sub_header):
     
@@ -74,15 +44,13 @@ def build_multi_index_data_frame(data_frames, stocks, columns):
 
 def get_portfolio_prices(stocks, funds, etfs, start_date, end_date=today):
     
-    data_frames_stocks = get_stock_data_frames(stocks, 'brazil', start_date=start_date, end_date=end_date)
-    data_frames_funds = get_funds_data_frames(funds, 'brazil', start_date=start_date, end_date=end_date)
-    data_frames_etfs = get_etfs_data_frames(etfs, 'brazil', start_date=start_date, end_date=end_date)
+    data_frames_stocks = get_assets_data_frames(stocks, inv.get_stock_historical_data, 'brazil', start_date=start_date, end_date=end_date)
+    data_frames_funds = get_assets_data_frames(funds, inv.get_fund_historical_data, 'brazil', start_date=start_date, end_date=end_date)
+    data_frames_etfs = get_assets_data_frames(etfs, inv.get_etf_historical_data, 'brazil', start_date=start_date, end_date=end_date)
     
     data_frames = [*data_frames_stocks, *data_frames_funds, *data_frames_etfs]
 
     assets = [*stocks, *funds, *etfs]
-
-    columns = ['Open', 'High', 'Low', 'Close']
     
     portfolio_prices = build_multi_index_data_frame(data_frames, assets, columns)
     
